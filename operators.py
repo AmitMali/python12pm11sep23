039ecea4123ddef2d8b81816d5ef825933b880db
AmitMali <amit23289@gmail.com>
2023-09-14 12:43:20 +0530

    intro to python and arithmatic operator

---
+++ operators.py
@@ -0,0 +1,11 @@
+#Aritmatic Operators
+no1=10
+no2=3
+
+print(f'{no1} + {no2}  = {no1+no2}')
+print(f'{no1} - {no2}  = {no1-no2}')
+print(f'{no1} * {no2}  = {no1*no2}')
+print(f'{no1} / {no2}  = {no1/no2}')
+print(f'{no1} // {no2}  = {no1//no2}')
+print(f'{no1} % {no2}  = {no1%no2}')
+print(f'{no1} ** {no2}  = {no1**no2}')
---
+++ variables_and_print.py
@@ -0,0 +1,6 @@
+# name="amit"
+# Name="rahul"
+# print("hello ",name,"how'z you doing today ?")
+# print("hello {} how'z you doing today ?".format(name))
+# print(f"hello {name} and {Name} how'z you doing today ?")
+

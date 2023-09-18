90c6d65a55a06c7eed8aad9796f76a6331142a00
AmitMali <amit23289@gmail.com>
2023-09-18 12:58:49 +0530

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+byear=int(input())
+# print(byear=="1989")
+cyear=2023
+
+age = cyear- byear
+# print(age)
+# print(type(byear))
+
+if age>=18 and age<=50:
+    print("you are eligible")
+else:
+    print("you are not eligible")
+
+
--- operators.py
+++ operators.py
@@ -1,11 +1,26 @@
 #Aritmatic Operators
-no1=10
-no2=3
+# no1=10
+# no2=3
+#
+# print(f'{no1} + {no2}  = {no1+no2}')
+# print(f'{no1} - {no2}  = {no1-no2}')
+# print(f'{no1} * {no2}  = {no1*no2}')
+# print(f'{no1} / {no2}  = {no1/no2}')
+# print(f'{no1} // {no2}  = {no1//no2}')
+# print(f'{no1} % {no2}  = {no1%no2}')
+# print(f'{no1} ** {no2}  = {no1**no2}')
 
-print(f'{no1} + {no2}  = {no1+no2}')
-print(f'{no1} - {no2}  = {no1-no2}')
-print(f'{no1} * {no2}  = {no1*no2}')
-print(f'{no1} / {no2}  = {no1/no2}')
-print(f'{no1} // {no2}  = {no1//no2}')
-print(f'{no1} % {no2}  = {no1%no2}')
-print(f'{no1} ** {no2}  = {no1**no2}')
+# print(2**3)
+
+# print( 102>20)
+# print( 102<20)
+# print(10>=10)
+# print(10<=10)
+# print(10==100)
+# print(10!=100)
+# no=124
+# print(no>10 and no < 20)
+# print(no>10 and no < 200)
+# print(no>10 or no <20)
+
+

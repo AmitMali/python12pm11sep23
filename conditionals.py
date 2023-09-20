49a2ee1a74cf3cdda7a99ec80145c27ffab090e4
AmitMali <amit23289@gmail.com>
2023-09-20 13:07:43 +0530

    string operations

--- conditionals.py
+++ conditionals.py
@@ -1,14 +1,22 @@
-byear=int(input())
+# byear=int(input())
 # print(byear=="1989")
-cyear=2023
+# cyear=2023
 
-age = cyear- byear
+# age = cyear- byear
 # print(age)
 # print(type(byear))
 
-if age>=18 and age<=50:
-    print("you are eligible")
-else:
-    print("you are not eligible")
+# if age>=18 and age<=50:
+#     print("you are eligible")
+# else:
+#     print("you are not eligible")
 
+no1,no2,no3=238,45,67
+
+if no1>no2 and no1>no3:
+    print(f'{no1} is greater')
+elif no2>no1 and no2>no3:
+    print(f'{no2} is greater')
+else:
+    print(f'{no3} is greater')
 
---
+++ strings.py
@@ -0,0 +1,22 @@
+foo="lets write some randome some string here"
+
+# print(foo[4:-4])
+
+# mysite="amitmali.com"
+# print(mysite[:-4])
+
+# print(foo[0:-1:3])
+# print(foo[::-1])
+
+# print(foo.count("s"))
+# print(foo.index("some"))
+# print(foo.split("o"))
+# print(foo.split("o")[1])
+
+# names=['amit','divakar','sushil','sanket']
+
+# print("-".join(names))
+
+print("khdjkahjkdkja".isalpha())
+print("1223233".isdecimal())
+print("      amit     mali     ".strip())
